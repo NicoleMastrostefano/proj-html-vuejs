b51261500aec21508ab99f43c51b87c1caaeb8d1
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-01-13 12:37:27 +0100

    carousel vue.js

--- js/script.js
+++ js/script.js
@@ -22,14 +22,59 @@ var app = new Vue(
           name: 'BLOG',
         }
       ],
-      active: ''
+
+      testimonials:[
+        {
+          name: 'Sophia Jones',
+          avatar:"img/testimonial-sophia.png",
+        },
+        {
+          name: 'Harold Green',
+          avatar:"img/testimonial-harold.png"
+        },
+        {
+          name:'Grant     Harvey',
+          avatar: "img/testimonial-grant.png",
+        },
+        {
+          name: 'Kate Lewis',
+          avatar:"img/testimonial-kate.png",
+        },
+        {
+          name: 'Kelly',
+          avatar:"img/testimonial-kelly.png",
+        },
+      ],
+      active: '',
+      avatarIndex:0,
+      autoplay:""
+    },
+
+    // funzione per autoplay avatar
+    created:function(){
+      this.autoplay = setInterval(
+        () => this.nextImage(),4000
+      )
     },
 
     methods: {
+      // funzione per attivare link navbar
+  		SetActive: function(newIndex){
+  			this.active = newIndex;
+  		},
 
-		SetActive: function(newIndex){
-			this.active = newIndex;
-		}
-	}
+      // funzione per attivare avatar
+      nextImage: function(){
+        this.avatarIndex ++;
+        if (this.avatarIndex==this.testimonials.length){
+          this.avatarIndex=0
+        }
+      },
+
+      // funzione per attivare il dot
+      dotActive: function(index){
+        (this.avatarIndex = index)
+      }
+	  }
   }
 )
